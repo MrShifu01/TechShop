3ecc4004fe1c2e830af7b52c56a09764a93633a9
MrShifu01 <stander.christian.com>
2023-06-08 08:48:08 +0200

    frontend looking solid

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,19 @@
-import React from 'react'
+import Footer from "./components/Footer"
+import Header from "./components/Header"
+import { Container } from "react-bootstrap"
+import { Outlet } from "react-router-dom"
 
 const App = () => {
   return (
-    <h1>Welcome to TechShop</h1>
+    <>
+      <Header/>
+      <main className="py-3">
+        <Container>
+          <Outlet/>
+        </Container>
+      </main>
+      <Footer/>
+    </>
   )
 }
 
