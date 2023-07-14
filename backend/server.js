acf6efec212e4b22d21d57cdd625130800971866
MrShifu01 <stander.christian.com>
2023-07-14 14:18:38 +0200

    multer add in the backend

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from 'path'
 import express from 'express'
 import dotenv from 'dotenv'
 dotenv.config()
@@ -6,6 +7,7 @@ import connectDB from './config/db.js'
 import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
 import orderRoutes from './routes/orderRoutes.js'
+import uploadRoutes from './routes/uploadRoutes.js'
 import cookieParser from 'cookie-parser'
 
 const PORT = process.env.PORT || 3001
@@ -25,10 +27,14 @@ app.get('/', (req, res) => {
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
+app.use('/api/upload', uploadRoutes)
 
 app.get('/api/config/paypal', (req, res) => 
     res.send({clientId: process.env.PAYPAL_CLIENT_ID})
 )
+
+const __dirname = path.resolve() //Set __dirname to current directory
+app.use('/uploads', express.static(path.join(__driname, '/uploads')))
 
 app.use(notFound)
 app.use(errorHandler)
