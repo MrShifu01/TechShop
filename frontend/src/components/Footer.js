7483a54e267a582857b66c0a822da3441190a041
MrShifu01 <stander.christian.com>
2023-07-17 13:09:17 +0200

    watches loaded into uploads

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -9,7 +9,7 @@ const Footer = () => {
         <Container>
             <Row>
                 <Col className="text-center py-3">
-                    <p>TechShop &copy; {currentYear}</p>
+                    <p>Shifu Watches &copy; {currentYear}</p>
                 </Col>
             </Row>
         </Container>
