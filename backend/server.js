7b3d783828b80624b51036f05bed7b2e1f633022
MrShifu01 <stander.christian.com>
2023-07-17 09:22:13 +0200

    TechShop Finished

--- backend/server.js
+++ backend/server.js
@@ -20,9 +20,7 @@ app.use(cookieParser())
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
-app.get('/', (req, res) => {
-    res.send("API is running...")
-})
+
 
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
@@ -35,6 +33,19 @@ app.get('/api/config/paypal', (req, res) =>
 
 const __dirname = path.resolve() //Set __dirname to current directory
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+
+if (process.env.NODE_ENV === 'production') {
+    // Set frontend/build as static folder
+    app.use(express.static(path.join(__dirname, '/frontend/build')))
+    // Serve index.html if any route is hit
+    app.get('*', (req, res) =>
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    )
+} else {
+    app.get('/', (req, res) => {
+        res.send("API is running...")
+    })
+}
 
 app.use(notFound)
 app.use(errorHandler)
