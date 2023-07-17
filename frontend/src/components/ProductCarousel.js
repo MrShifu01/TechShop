cbebe4a20703e9d33568332f78019d2ba3e08f41
MrShifu01 <stander.christian.com>
2023-07-17 12:19:40 +0200

    a

--- frontend/src/components/ProductCarousel.js
+++ frontend/src/components/ProductCarousel.js
@@ -16,7 +16,7 @@ const ProductCarousel = () => {
             <Image className='carousel-image' src={product.image} alt={product.name} fluid />
             <Carousel.Caption className='carousel-caption'>
               <h2 className='text-white text-right'>
-                {product.name} (${product.price})
+                {product.name} (R{product.price})
               </h2>
             </Carousel.Caption>
           </Link>
